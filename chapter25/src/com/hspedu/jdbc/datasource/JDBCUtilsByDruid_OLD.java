a9de6944e6c23997ba354031b2d0ce2b56f29865
Oxidaner <18622412361@163.com>
2021-12-29 03:13:02 +0800

    jdbc连接池

--- chapter25/src/com/hspedu/jdbc/datasource/JDBCUtilsByDruid_OLD.java
+++ chapter25/src/com/hspedu/jdbc/datasource/JDBCUtilsByDruid_OLD.java
@@ -1,57 +1,26 @@
 package jdbc.datasource;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
- * @author 韩顺平
- * @version 1.0
+ * @author A
+ * @date 2021/12/29 - 12 - 29 - 3:05
+ * jdbc.datasource
  */
-@SuppressWarnings({"all"})
-public class JDBCUtilsByDruid_USE {
-
-    @Test
-    public void testSelect() {
-
-        System.out.println("使用 druid方式完成");
-        //1. 得到连接
-        Connection connection = null;
-        //2. 组织一个sql
-        String sql = "select * from actor where id >= ?";
-        PreparedStatement preparedStatement = null;
-        ResultSet set = null;
-        //3. 创建PreparedStatement 对象
-        try {
-            connection = JDBCUtilsByDruid.getConnection();
-            System.out.println(connection.getClass());//运行类型 com.alibaba.druid.pool.DruidPooledConnection
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, 1);//给?号赋值
-            //执行, 得到结果集
-            set = preparedStatement.executeQuery();
-
-            //遍历该结果集
-            while (set.next()) {
-                int id = set.getInt("id");
-                String name = set.getString("name");//getName()
-                String sex = set.getString("sex");//getSex()
-                Date borndate = set.getDate("borndate");
-                String phone = set.getString("phone");
-                System.out.println(id + "\t" + name + "\t" + sex + "\t" + borndate + "\t" + phone);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            //关闭资源
-            JDBCUtilsByDruid.close(set, preparedStatement, connection);
-        }
+public class JDBCUtilsByDruid_OLD {
+    public static void main(String[] args) {
+        testSelectToArrayList();
     }
-
-
     //使用老师的土方法来解决ResultSet =封装=> Arraylist
     @Test
-    public void testSelectToArrayList() {
+    public static ArrayList<Actor> testSelectToArrayList() {
 
         System.out.println("使用 druid方式完成");
         //1. 得到连接
@@ -95,7 +64,7 @@ public class JDBCUtilsByDruid_USE {
             JDBCUtilsByDruid.close(set, preparedStatement, connection);
         }
         //因为ArrayList 和 connection 没有任何关联，所以该集合可以复用.
+        return list;
 
     }
-
 }
